9492bbd0ec3fd623f2e45f09f3897bd90179d289
arfento <arfento@gmail.com>
2021-04-06 00:13:58 +0700

    menambahkan command add menggunakan yard WPU#11

--- app.js
+++ app.js
@@ -1,4 +1,17 @@
-const contacts = require('./contacts');
+///mengambil argumen dari command line
+/* 
+const command = process.argv[2];
+if (command === "add"){
+
+} else if (command === "remove"){
+
+}else if (command === "list"){
+
+}
+ */
+
+
+/* const contacts = require('./contacts');
 //atau menggunakan 
 
 // const {tulisPertanyaan, simpanContact} = require('./contacts');
@@ -16,4 +29,56 @@ const main = async () => {
 
 };
 
-main();
+main(); */
+
+
+
+///argumen menggunakan yargs
+
+const yargs = require("yargs");
+const contacts = require("./contacts");
+
+/* yargs.command(
+    'add', 
+    'menambahkan contact baru',
+     () => {}, 
+     (argv) => {
+        console.log(argv.nama);
+    }
+); */
+
+
+//parameter dalam bentuk object
+yargs.command({
+    command : 'add',
+    describe : 'menambahkan contact baru',
+    builder : {
+        nama : {
+            describe : 'Nama Lengkap',
+            demandOption : true,
+            type: 'string',
+        },
+        noHP : {
+            describe : 'No HP',
+            demandOption : true,
+            type : 'string'
+        },
+        alamat : {
+            describe: 'Alamat',
+            demandOption : false,
+            type : 'string',
+        },
+    },
+    handler : function(argv){
+        /* const contact = {
+            nama : argv.nama,
+            noHP : argv.noHP,
+            alamat : argv.alamat,
+        };
+        console.log(contact); */
+        contacts.simpanContact(argv.nama, argv.noHP, argv.alamat);
+    }
+
+});
+
+yargs.parse();
--- contacts.js
+++ contacts.js
@@ -1,12 +1,15 @@
 const fs = require('fs');
+const chalk = require('chalk');
+const validator = require('validator');
 
-const readline = require('readline');
+//tidak digunakan lagi setalah menggunakan yargs
+/* const readline = require('readline');
 
 
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
-});
+}); */
 
 const dirPath = './data';
 if (!fs.existsSync(dirPath)) {
@@ -19,15 +22,15 @@ if (!fs.existsSync(dataPath)) {
     fs.writeFileSync(dataPath, '[]', 'utf-8');
     console.log('File Contacts telah berhasil dibuat');
 }
-
-const tulisPertanyaan = (pertanyaan) => {
+//tulis pertnyaan tidak terpakai
+/* const tulisPertanyaan = (pertanyaan) => {
     return new Promise((resolve, reject) => {
         rl.question(pertanyaan, (tulis) =>{
             resolve(tulis);
         });
     });
 
-};
+}; */
 
 
 const simpanContact = (nama, noHP, alamat) => {
@@ -39,15 +42,38 @@ const simpanContact = (nama, noHP, alamat) => {
     const fileBuffer = fs.readFileSync('data/contacts.json', 'utf-8', (err, data) => {});
 
     const contacts = JSON.parse(fileBuffer);
+
+
+    //cek duplikat
+    const duplikat = contacts.find((contact) => contact.nama === nama );
+    if(duplikat){
+        console.log(chalk.red.inverse.bold("contact sudah terdaftar!!, Gunakan nama lain"));
+        return false;
+    }
+
+    // cek email / data kurang 
+    /* if(email){
+        if (!validator.isEmail(email)){
+            console.log(chalk.red.inverse.bold("Email tidak valid!"));
+            return false;
+        }
+    } */
+
+    if (!validator.isMobilePhone(noHP, 'id-ID')){
+        console.log(chalk.red.inverse.bold("Nomor HP tidak valid!"));
+        return false;
+    }
+
+
     contacts.push(contact);
 
     fs.writeFileSync('data/contacts.json', JSON.stringify(contacts));
-    console.log('TerimaKasih telah memasukkan data.');
-    rl.close();
+    console.log(chalk.green.inverse.bold('TerimaKasih telah memasukkan data.'));
+    // rl.close();
 };
 
 
 module.exports = {
-    tulisPertanyaan : tulisPertanyaan,
+    // tulisPertanyaan : tulisPertanyaan,
     simpanContact : simpanContact,
 };
